963bc70170cab3fe36d38b7f8af0e140cb45d97f
5kipp3r <112339635+jayvin1244@users.noreply.github.com>
2022-10-27 18:38:36 +0530

    Add files via upload

---
+++ addition of 2 number.py
@@ -0,0 +1,36 @@
+n1=input("enter first number")
+n2=input ("enter second number")
+total=int(n1)+int(n2)
+print("total=",total)
+# run time initialization and 2 num of addition 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ addition.py
@@ -0,0 +1,14 @@
+print (2+3*4)
+print (2/4) # floting point divition
+print  (4/2)
+print (4//2) # integer divition
+print (2**3)  #cub of 2 
+print (2**0.5)
+print (round(2**0.5, 4)) # round up the desimal number
+print (2**3/2*6-4*(3-4/2))
+print (3%2)       
+print ((2+3)/2)
+print ((2+3)//2)
+print ((2+3)*5/2%6)
+print (12.5 % 6)
+print (2**(3**2))
